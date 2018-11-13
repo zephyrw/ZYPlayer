78c2618494f6109f5ea366a822b635b426ea53eb
朱欣旺 <zhuxinwang@cmiot.chinamobile.com>
2018-11-13 11:26:15 +0800

    Initialize

---
+++ Example/Example/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Example
+//
+//  Created by Zephyr on 2018/11/13.
+//  Copyright © 2018 zephyr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
